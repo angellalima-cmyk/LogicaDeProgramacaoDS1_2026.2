a86c655f342009ed41bc3e3efe1d97c81297209a
unknown <angella.lima@aluno.ce.gov.br>
2026-09-14 14:48:39 -0300

    atividade 01

---
+++ 01_nocoes_e_fundamentos/olamundo.py
@@ -0,0 +1,15 @@
+print ("Angella virginia")
+print ("ola mundo!")
+
+#receba um nome
+#receba dia de nascimento 
+#receba mes de nascimento 
+#receba ano de nascimento 
+#mostre tudo
+
+nome = input("digite seu nome")
+dia = input("digite o dia de nascimento")
+mes = input("digite o mes de nascimento")
+ano = input("digite o ano do nascimento")
+
+print("seu nome é" (nome) você nasceu (dia) você nasceu (mes) você nasceu (ano))
---
+++ 02_conceitos_basicos/atividade_aula 01.py
@@ -0,0 +1,11 @@
+# TODO: Declare as variáveis abaixo e execute a célula para testar
+nome_produto = "notebook"
+quantidade_estoque = 120
+preco_unitario = 32.9
+disponivel_para_venda = False
+# TODO: Escreva os prints de inspeção aqui
+
+print("tipo de nome:", type(nome_produto))
+print("tipo de quantidade:", type(quantidade_estoque))
+print("tipo de preco:", type(preco_unitario))
+print("tipo disponivel:", type(disponivel_para_venda))
